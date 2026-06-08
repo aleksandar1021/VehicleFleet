10a577cef370c86e3ce6d7965d9dd58d25421f4c
Aleksandar Kostic <aleksandar@b4c.rs>
2026-06-08 10:41:12 +0000

    test

--- pages/listOfPlannedEvents/jsobjects/onRowSelectJS/onRowSelectJS.js
+++ pages/listOfPlannedEvents/jsobjects/onRowSelectJS/onRowSelectJS.js
@@ -1,8 +1,11 @@
 export default {
   Table1onRowSelected: async () => {
-    await storeValue('previewRow', Table1.selectedRow);
-		await GetPlannedEventsByVehicle.run();
-
-    showModal('singlePlannedEventView');
-  }
+  await storeValue('previewRow', Table1.selectedRow);
+  await storeValue('previewVehicleName', Table1.selectedRow.vehicles?.equipment_name || Table1.selectedRow.vehicles);
+  await storeValue('previewVehicleImage', Table1.selectedRow.vehicles?.image_url);
+  await GetPlannedEventsByVehicle.run();
+  showModal('singlePlannedEventView');
+		
+		console.log(Table1.selectedRow)
+}
 }
