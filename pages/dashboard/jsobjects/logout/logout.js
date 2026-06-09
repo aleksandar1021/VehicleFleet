e0a72d977045f6c22add88baa434ed7ca1db7179
Aleksandar Kostic <aleksandar@b4c.rs>
2026-06-09 11:08:03 +0000

    t

--- pages/dashboard/jsobjects/logout/logout.js
+++ pages/dashboard/jsobjects/logout/logout.js
@@ -6,8 +6,10 @@ export default {
         }
 
         const role = (appsmith.store.korisnikRole || '').toLowerCase();
+					console.log("ULOGAA:" + role)
 
         if (role !== 'admin' && role !== 'menadzer') {
+					console.log("ULOGAA:" + role)
 						storeValue('iframeUrl', 'https://app.appsmith.com/app/vehiclefleet/blankpage-6a199460238152f20247be0f')
 						return;
         }
