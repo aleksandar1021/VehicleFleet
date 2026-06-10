bf01dee3775981363deba2d00022393ea34a77fc
Aleksandar Kostic <aleksandar@b4c.rs>
2026-06-10 07:06:41 +0000

    end

--- pages/listOfUsers/jsobjects/Logout/Logout.js
+++ pages/listOfUsers/jsobjects/Logout/Logout.js
@@ -1,15 +1,15 @@
 export default {
     checkLogin: async () => {
-        if (!appsmith.store.logovan) {
-            navigateTo('login');
-            return;
-        }
+        // if (!appsmith.store.logovan) {
+            // navigateTo('login');
+            // return;
+        // }
 
-        const role = (appsmith.store.korisnikRole || '').toLowerCase();
-
-        if (role !== 'admin' && role !== 'menadzer') {
-            navigateTo('blankPage');
-            return;
-        }
+        // const role = (appsmith.store.korisnikRole || '').toLowerCase();
+// 
+        // if (role !== 'admin' && role !== 'menadzer') {
+            // navigateTo('blankPage');
+            // return;
+        // }
     }
 }
